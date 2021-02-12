ea1b657a2d0db26c17b362f7b1a6dae4f14c626f
vincent51689453 <vincent51689453@gmail.com>
2021-02-13 04:10:07 +0800

    changed optimizer

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
-epoches = 1500
+epoches = 1100
 batch_size = 64
 initial_learning_rate = 0.001
 decay_step = 100
@@ -18,24 +18,24 @@ def Neural_Network():
     neurons = 500
 
     #Input layer
-    model.add(Dense(neurons, input_dim=1, activation='relu', kernel_initializer='normal'))
+    model.add(Dense(neurons, input_dim=1, activation='tanh', kernel_initializer='normal'))
     
     #Hidden layer
-    model.add(Dense(int(neurons-50*1),input_dim = neurons,activation='relu', kernel_initializer='normal'))
+    model.add(Dense(int(neurons-20*1),input_dim = neurons,activation='tanh', kernel_initializer='normal'))
     model.add(Dropout(0.15))
-    model.add(Dense(int(neurons-50*2),input_dim = int(neurons-50*1),activation='relu', kernel_initializer='normal'))
+    model.add(Dense(int(neurons-20*2),input_dim = int(neurons-20*1),activation='tanh', kernel_initializer='normal'))
     model.add(Dropout(0.15))
-    model.add(Dense(int(neurons-50*3),input_dim = int(neurons-50*2),activation='relu', kernel_initializer='normal'))
+    model.add(Dense(int(neurons-20*3),input_dim = int(neurons-20*2),activation='tanh', kernel_initializer='normal'))
     model.add(Dropout(0.15))
-    model.add(Dense(int(neurons-50*4),input_dim = int(neurons-50*3),activation='relu', kernel_initializer='normal'))
+    model.add(Dense(int(neurons-20*4),input_dim = int(neurons-20*3),activation='tanh', kernel_initializer='normal'))
     model.add(Dropout(0.15))
-    model.add(Dense(int(neurons-50*5),input_dim = int(neurons-50*4),activation='relu', kernel_initializer='normal'))
+    model.add(Dense(int(neurons-20*5),input_dim = int(neurons-20*4),activation='tanh', kernel_initializer='normal'))
     model.add(Dropout(0.15))
-    model.add(Dense(int(neurons-50*6),input_dim = int(neurons-50*5),activation='relu', kernel_initializer='normal'))
+    model.add(Dense(int(neurons-20*6),input_dim = int(neurons-20*5),activation='tanh', kernel_initializer='normal'))
     model.add(Dropout(0.15))
 
     #Output layer
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1, activation='linear'))
 
     return model
 
@@ -53,9 +53,9 @@ def model_training(training_x,training_y,testing_x,testing_y,raw_index,raw_price
         decay_rate=decay_factor,
         staircase=True)
 
-    #opt = keras.optimizers.Adam(learning_rate=lr_schedule)
+    opt = keras.optimizers.Adam(learning_rate=lr_schedule)
     #opt = keras.optimizers.SGD(learning_rate=lr_schedule,momentum=0.9)
-    opt = keras.optimizers.SGD(learning_rate=lr_schedule,momentum=0.9)
+    #opt = keras.optimizers.SGD(learning_rate=lr_schedule,momentum=0.9)
     model.compile(loss=loss_function, optimizer=opt)
     history = model.fit(training_x, training_y, epochs=epoches, batch_size=batch_size, validation_data=(testing_x, testing_y), verbose=2)
 
@@ -77,7 +77,7 @@ def model_training(training_x,training_y,testing_x,testing_y,raw_index,raw_price
     #Prediction
     y_pred = model.predict(raw_index)
     fig = plt.figure(figsize=(15,8))
-    plt.title('AI Prediction of EURUSD')
+    plt.title('AI Stock Price Prediction')
     plt.scatter(raw_index, raw_price, c='green', label='Market',s=5)
     plt.scatter(raw_index, y_pred, c='red', label='AI-Prediction',s=5)
     plt.legend()
