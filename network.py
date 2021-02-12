dea0d00d411e7886727f411b405ee1b2d803c463
vincent51689453 <vincent51689453@gmail.com>
2021-02-13 02:41:13 +0800

    save data module completed

---
+++ network.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Input, concatenate
+from tensorflow.keras.models import Model
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+epoches = 300
+batch_size = 8
+initial_learning_rate = 0.01
+decay_step = 50
+decay_factor = 0.96
+
+def Neural_Network():
+    model = Sequential()
+    neurons_num = 80
+
+    #Input layer
+    model.add(Dense(1, input_dim=1, activation='tanh'))
+    
+    #Hidden layer
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+    model.add(Dense(neurons_num, activation='tanh'))
+    model.add(Dropout(0.15))
+
+    #Output layer
+    model.add(Dense(1, activation='sigmoid'))
+
+    return model
+
+def model_training(training_x,training_y,testing_x,testing_y,raw_index,raw_price):
+    #Start AI Analysis
+    loss_function = tf.keras.losses.MeanSquaredError()    
+
+    #Create Network, remember to change the network with different dataset
+    model = Neural_Network()
+
+    # fit the model on the training dataset
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate,
+        decay_steps=decay_step ,
+        decay_rate=decay_factor,
+        staircase=True)
+
+    #opt = keras.optimizers.Adam(learning_rate=lr_schedule)
+    #opt = keras.optimizers.SGD(learning_rate=lr_schedule,momentum=0.9)
+    opt = keras.optimizers.SGD(learning_rate=lr_schedule,momentum=0.9)
+    model.compile(loss=loss_function, optimizer=opt)
+    history = model.fit(training_x, training_y, epochs=epoches, batch_size=batch_size, validation_data=(testing_x, testing_y), verbose=2)
+
+    print()
+    print("History keys are following: ")
+    print(history.history.keys())
+    print()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['loss', 'validation loss'], loc='upper left')
+    plt.show()
+
+
+    #Prediction
+    print(raw_index)
+    y_pred = model.predict(raw_index)
+    fig = plt.figure(figsize=(15,8))
+    plt.title('AI Prediction of EURUSD')
+    plt.scatter(raw_index, raw_price, c='green', label='Market',s=2)
+    plt.scatter(raw_index, y_pred, c='red', label='AI-Prediction',s=2)
+    plt.legend()
+    plt.grid
+    plt.show()
--- save_data.py
+++ save_data.py
@@ -4,20 +4,30 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from network import *
+from random import shuffle
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import csv
+
 
 #Company
 target_stock = 'wkhs'
 #Sampling interval
 intervals_seconds = 1
-#Analyze window size
-window_size = 100
 #Buffers
 price_buffer = []
 time_buffer = []
+index_buffer = []
 #Plot
 marker_color = 'b'
 marker_size = 20
 marker_shape = 'x'
+#CSV data
+file_path = 'stock.csv'
+csv_index = 0
 
 
 
@@ -34,33 +44,37 @@ def realtime_plot(time_x,price_y):
     plt.autoscale(enable=True, axis='both')
     y = np.array(price_y)
     x = np.array(time_x)
-    plt.title('AI Estimator:'+target_stock)
+    plt.title('Stock Price :'+target_stock)
     plt.xlabel("Time")
     plt.ylabel("Price")
     plt.scatter(x,y,s=marker_size,c=marker_color,marker=marker_shape)
     plt.pause(intervals_seconds)
+
     
 
 def main():
-    global price_buffer,window_size
+    global price_buffer,window_size,csv_index,index_buffer
     counter = 0
     while True:
+        #Read stock prices
         systime = get_systime()
         price = get_stockprice_now(target_stock)
-
-        if(counter<window_size):
-            price_buffer.append(price)
-            time_buffer.append(systime)
-            print("Time: "+systime+"| Price: "+str(price)+"|  Buffer_index: "+str(counter))
-            if(counter>0):
-                realtime_plot(time_buffer,price_buffer)
-            counter += 1
-        else:
-            price_buffer = []
-            counter = 0
+        csv_list = [systime,price,csv_index]
+        csv_index += 1
 
 
-        #time.sleep(intervals_seconds)
+        #Save to csv for dataset
+        with open(file_path, 'a',newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(csv_list)
+        
+        #Realtime plot
+        price_buffer.append(price)
+        time_buffer.append(systime)
+        index_buffer.append(csv_index)
+        print("Time: "+systime+"| Price: "+str(price)+"|  Buffer_index: "+str(counter))
+        realtime_plot(index_buffer,price_buffer)
+        
     plt.show()
 
 
